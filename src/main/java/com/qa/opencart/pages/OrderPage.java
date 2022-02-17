dd5d62f22b2b4d5b12fcb7bef3b3a15b2ed032a7
Sushma <sushmap21@gmail.com>
2022-02-17 16:59:46 -0600

    order page is updated as per Naveens comments

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,9 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
 	
 	public void order() {
-		System.out.println("Order");
+		By order=By.id("input");
+		System.out.println("Order id:	12345");
 	}
 
 }
